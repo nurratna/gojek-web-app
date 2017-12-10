4e63375296da58dc5a690071c84ff5ef85aad3e1
nurratna <nurratnasarii@gmail.com>
2017-12-11 04:23:10 +0700

    validating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :password, length: { minimum: 8 }, allow_blank: true
+  validates :gopay, numericality: { greater_than_or_equal_to: 0 }
+
+  def topup(amount)
+    if !(is_numeric?(amount))
+      errors.add(:gopay, "is not a number")
+    elsif amount.to_i < 0
+      errors.add(:gopay, "must be greater than or equal to 0")
+    else
+      update(gopay: gopay + amount.to_i)
+    end
+  end
+
+  private
+    def is_numeric?(obj)
+      obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+    end
 end
