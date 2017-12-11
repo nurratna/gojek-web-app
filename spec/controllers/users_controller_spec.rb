374c26daf37efdb6fa62b55dc6545cbf659e0092
nurratna <nurratnasarii@gmail.com>
2017-12-11 13:51:37 +0700

    adding users controller spec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,129 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested user to @user' do
+      user = create(:user)
+      get :show, params: { id: user }
+      expect(assigns(:user)).to eq(user)
+      # expect(user).to eq(true)
+    end
+
+    it 'renders the :show template' do
+      user = create(:user)
+      get :show, params: { id: user }
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'assigns a new User to @user' do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it 'renders the :new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'GET #edit' do
+    it "assigns the requested user to @user" do
+      user = create(:user)
+      get :edit, params: { id: user }
+      expect(assigns(:user)).to eq user
+    end
+
+    it "renders the :edit template" do
+      user = create(:user)
+      get :edit, params: { id: user }
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it "saves the new user in the database" do
+        expect{
+          post :create, params: { user: attributes_for(:user) }
+        }.to change(User, :count).by(1)
+      end
+
+      it "redirects to users#index" do
+        post :create, params: { user: attributes_for(:user) }
+        expect(response).to redirect_to users_url
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new user in the database" do
+        expect{
+          post :create, params: { user: attributes_for(:invalid_user) }
+        }.not_to change(User, :count)
+      end
+
+      it "re-renders the :new template" do
+        post :create, params: { user: attributes_for(:invalid_user) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    before :each do
+      @user = create(:user, password: 'oldpassword', password_confirmation: 'oldpassword')
+    end
+
+    context "with valid attributes" do
+      it "locates the requested @user" do
+        patch :update, params: { id: @user, user: attributes_for(:user) }
+        expect(assigns(:user)).to eq(@user)
+      end
+
+      it "saves new password" do
+        patch :update, params: { id: @user, user: attributes_for(:user, password: 'newlongpassword', password_confirmation: 'newlongpassword') }
+        @user.reload
+        expect(@user.authenticate('newlongpassword')).to eq(@user)
+      end
+
+      it "redirects to users#index" do
+        patch :update, params: { id: @user, user: attributes_for(:user) }
+        expect(response).to redirect_to users_url
+      end
+
+      it "disables login with old password" do
+        patch :update, params: { id: @user, user: attributes_for(:user, password: 'newlongpassword', password_confirmation: 'newlongpassword') }
+        @user.reload
+        expect(@user.authenticate('oldpassword')).to eq(false)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not update the user in the database" do
+        patch :update, params: { id: @user, user: attributes_for(:user, password: nil, password_confirmation: nil) }
+        @user.reload
+        expect(@user.authenticate(nil)).to eq(false)
+      end
+
+      it "re-renders the :edit template" do
+        patch :update, params: { id: @user, user: attributes_for(:invalid_user) }
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+end
