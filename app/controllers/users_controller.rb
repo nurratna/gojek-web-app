73554ca6187b858127dcf090b82e580adafff1aa
nurratna <nurratnasarii@gmail.com>
2017-12-11 15:01:29 +0700

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,70 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
   def index
+    @users = User.all
   end
+
+  # GET /users/1
+  def show
+  end
+
+  # GET users/1/edit
+  def new
+    @user = User.new
+  end
+
+  # GET /users/new
+  def edit
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_url, notice: 'User was successfully created.'}
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_url, notice: 'User was successfully updated.'}
+        format.json { render :show, status: :ok, location: @user }
+
+        # @users = User.all
+        # ActionCable.server.broadcast 'users', html: render_to_string('store/index', layout: false)
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "User was successfully destroy" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :phone, :password, :passsword_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  # get 'users/index'
 
   # get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index', as: 'home_index'
+  resources :users
 end
