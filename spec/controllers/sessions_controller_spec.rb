0ae4d4b57638f4cfb0f23f1b456ecf6b7ee53d2d
nurratna <nurratnasarii@gmail.com>
2017-12-17 06:55:18 +0700

    adding spec can't access login page when logged in

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -25,7 +25,7 @@ describe SessionsController do
         logout_user
         logout_driver
         get :new
-        expect(response).to render_template :new
+        expect(respons  e).to render_template :new
       end
     end
   end
