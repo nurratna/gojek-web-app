e2d74de3d98e3513c748e12308e3e04271475868
nurratna <nurratnasarii@gmail.com>
2017-12-17 22:40:00 +0700

    removing file

--- db/migrate/20171213032912_add_user_id_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToOrders < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :orders, :user, foreign_key: true
-  end
-end
--- db/migrate/20171216235704_add_token_to_drivers.rb
+++
@@ -1,5 +0,0 @@
-class AddTokenToDrivers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :drivers, :token, :string
-  end
-end
